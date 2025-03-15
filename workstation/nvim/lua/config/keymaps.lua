87283dd12cfcb1f82b9f6a6aebec640a083cbc47
Francis Meetze <francis@odysseyalive.com>
2025-03-14 18:28:36 -0700

    lazyvim keymap update

--- workstation/nvim/lua/config/keymaps.lua
+++ workstation/nvim/lua/config/keymaps.lua
@@ -3,15 +3,22 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>r", ":FzfLua resume<CR>", { desc = "Resume" })
 
+-- orgmode
+vim.keymap.set("n", "<leader>o", "<Nop>", { desc = "OrgMode" })
+
+-- tables mode
+vim.keymap.set("n", "<leader>t", "<Nop>", { desc = "TableMode" })
+
 -- Copilot Chat keymaps
-vim.keymap.set({ "n", "v" }, "<leader>ac", ":CopilotChat<CR>", { desc = "CopilotChat - Open Prompt" })
-vim.keymap.set("n", "<leader>ae", ":CopilotChatExplain<CR>", { desc = "CopilotChat - Explain code" })
-vim.keymap.set("n", "<leader>at", ":CopilotChatTests<CR>", { desc = "CopilotChat - Generate tests" })
-vim.keymap.set("n", "<leader>ar", ":CopilotChatReview<CR>", { desc = "CopilotChat - Review code" })
-vim.keymap.set("n", "<leader>af", ":CopilotChatFix<CR>", { desc = "CopilotChat - Fix code" })
-vim.keymap.set("n", "<leader>ai", ":CopilotChatImplement<CR>", { desc = "CopilotChat - Implement code" })
-vim.keymap.set("n", "<leader>ad", ":CopilotChatDebug<CR>", { desc = "CopilotChat - Debug code" })
-vim.keymap.set("n", "<leader>ao", ":CopilotChatOptimize<CR>", { desc = "CopilotChat - Optimize code" })
-vim.keymap.set("v", "<leader>ae", ":CopilotChatExplain<CR>", { desc = "CopilotChat - Explain selected code" })
-vim.keymap.set("v", "<leader>ar", ":CopilotChatReview<CR>", { desc = "CopilotChat - Review selected code" })
-vim.keymap.set("v", "<leader>at", ":CopilotChatTests<CR>", { desc = "CopilotChat - Generate tests for selected code" })
+vim.keymap.set("n", "<leader>a", "<Nop>", { desc = "Copilot" })
+vim.keymap.set({ "n", "v" }, "<leader>ac", ":CopilotChat<CR>", { desc = "Open Prompt" })
+vim.keymap.set("n", "<leader>ae", ":CopilotChatExplain<CR>", { desc = "Explain code" })
+vim.keymap.set("n", "<leader>at", ":CopilotChatTests<CR>", { desc = "Generate tests" })
+vim.keymap.set("n", "<leader>ar", ":CopilotChatReview<CR>", { desc = "Review code" })
+vim.keymap.set("n", "<leader>af", ":CopilotChatFix<CR>", { desc = "Fix code" })
+vim.keymap.set("n", "<leader>ai", ":CopilotChatImplement<CR>", { desc = "Implement code" })
+vim.keymap.set("n", "<leader>ad", ":CopilotChatDebug<CR>", { desc = "Debug code" })
+vim.keymap.set("n", "<leader>ao", ":CopilotChatOptimize<CR>", { desc = "Optimize code" })
+vim.keymap.set("v", "<leader>ae", ":CopilotChatExplain<CR>", { desc = "Explain selected code" })
+vim.keymap.set("v", "<leader>ar", ":CopilotChatReview<CR>", { desc = "Review selected code" })
+vim.keymap.set("v", "<leader>at", ":CopilotChatTests<CR>", { desc = "Generate tests for selected code" })
