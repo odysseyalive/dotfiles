218c54347ed4d51ffa584b2342757bb41a6ede08
Francis Meetze <francis@odysseyalive.com>
2025-07-01 22:31:52 -0700

    workstation update

--- workstation/nvim/lua/plugins/php.lua
+++ workstation/nvim/lua/plugins/php.lua
@@ -9,4 +9,35 @@ return {
       },
     },
   },
+
+  -- Disable PHPCS in none-ls (formerly null-ls)
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      -- Remove PHPCS from the sources if it exists
+      if opts.sources then
+        opts.sources = vim.tbl_filter(function(source)
+          return source.name ~= "phpcs"
+        end, opts.sources)
+      end
+      return opts
+    end,
+  },
+
+  -- Configure LSP preferences
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        phpactor = {
+          mason = false,
+          enabled = false,
+        },
+        intelephense = {
+          enabled = true,
+        },
+      },
+    },
+  },
 }
