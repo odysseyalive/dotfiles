46c39e3dac7686eabd074f2045a6b49d187cfe9e
Francis Meetze <francis@odysseyalive.com>
2025-09-10 10:52:53 -0700

    gruvbox light update

--- workstation/nvim/lua/plugins/gruvbox.lua
+++ workstation/nvim/lua/plugins/gruvbox.lua
@@ -1,7 +1,18 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        contrast = "soft", -- "hard" or "soft" or ""
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+    end,
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
