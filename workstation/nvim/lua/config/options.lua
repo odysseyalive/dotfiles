a6a46e8305d527bc5dcb1990a82cc99448e6964c
Francis Meetze <francis@odysseyalive.com>
2025-09-25 19:20:48 -0700

    assume color of terminal

--- workstation/nvim/lua/config/options.lua
+++ workstation/nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.background = "dark"
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 vim.g.lazyvim_php_lsp = "intelephense"
