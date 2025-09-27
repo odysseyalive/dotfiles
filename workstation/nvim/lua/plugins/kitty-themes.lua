af62b8b1a76ae3f261a2372a8c06d3a83f081bc1
Francis Meetze <francis@odysseyalive.com>
2025-09-27 14:02:30 -0700

    adds kitty themes

---
+++ workstation/nvim/lua/plugins/kitty-themes.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "odysseyalive/kitty-themes.nvim",
+    config = function()
+      require("kitty-themes").setup({
+        -- Configuration options
+        transparent = false,
+        term_colors = true,
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "SeaShells",
+    },
+  },
+}
